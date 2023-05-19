d4a398c46f538397c9d5afddc723f2fff0a4c306
E. Hannah <echannah631@gmail.com>
2023-05-19 11:48:57 -0500

    added second column to hero section that displayed mockups, added css effects to those mockups (🔥🔥🔥)

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -3,6 +3,7 @@ import styled from "styled-components"
 import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
 import PurchaseButton from "../buttons/PurchaseButton"
+import MockupAnimation from "../animations/MockupAnimation"
 
 const Hero = props => {
   return (
@@ -21,6 +22,7 @@ const Hero = props => {
             subtitle="120+ hours of video"
           />
         </TextWrapper>
+        <MockupAnimation />
       </ContentWrapper>
     </Wrapper>
   )
@@ -30,12 +32,15 @@ export default Hero
 
 const Wrapper = styled.div`
   background: linear-gradient(180deg, #4316db 0%, #9876e7 100%);
+  overflow: hidden;
 `
 
 const ContentWrapper = styled.div`
   max-width: 1234px;
   margin: 0 auto;
   padding: 200px 30px;
+  display: grid;
+  grid-template-columns: 360px auto;
 `
 
 const TextWrapper = styled.div`
