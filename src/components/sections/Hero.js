e126183820056d22b2caa616698625bc8c9661a6
E. Hannah <echannah631@gmail.com>
2023-05-19 00:08:06 -0500

    used styled components in hero

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -1,18 +1,50 @@
 import React from "react"
 import { Link } from "gatsby"
+import styled from "styled-components"
 
 const Hero = props => {
   return (
-    <div>
-      <img src="/images/logos/logo.svg" alt="logo" />
-      <h1>Design and code React apps</h1>
-      <p>
-        Don’t skip design. Learn design and code, by building real apps with
-        React and Swift. Complete courses about the best tools.
-      </p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-    </div>
+    <Wrapper>
+      <ContentWrapper>
+        <TextWrapper>
+          <Title>
+            Design <br /> and code React apps
+          </Title>
+          <Description>
+            Don’t skip design. Learn design and code, by building real apps with
+            React and Swift. Complete courses about the best tools.
+          </Description>
+        </TextWrapper>
+      </ContentWrapper>
+    </Wrapper>
   )
 }
 
 export default Hero
+
+const Wrapper = styled.div`
+  background: linear-gradient(180deg, #4316db 0%, #9876e7 100%);
+`
+
+const ContentWrapper = styled.div`
+  max-width: 1234px;
+  margin: 0 auto;
+  padding: 200px 30px;
+`
+
+const TextWrapper = styled.div`
+  max-width: 360px;
+  display: grid;
+  gap: 30px;
+`
+
+const Title = styled.h1`
+  font-weight: bold;
+  font-size: 60px;
+  color: white;
+`
+
+const Description = styled.p`
+  font-size: 1.1rem;
+  line-height: 130%;
+`
