bfad106be8733ce9b7df4701c34908880a4580c2
E. Hannah <echannah631@gmail.com>
2023-05-19 10:30:57 -0500

    created purchase button

--- src/components/sections/Hero.js
+++ src/components/sections/Hero.js
@@ -2,6 +2,7 @@ import React from "react"
 import styled from "styled-components"
 import { H1, MediumText } from "../styles/TextStyles"
 import { themes } from "../styles/ColorStyles"
+import PurchaseButton from "../buttons/PurchaseButton"
 
 const Hero = props => {
   return (
@@ -15,6 +16,10 @@ const Hero = props => {
             Don’t skip design. Learn design and code, by building real apps with
             React and Swift. Complete courses about the best tools.
           </Description>
+          <PurchaseButton
+            title="Start learning"
+            subtitle="120+ hours of video"
+          />
         </TextWrapper>
       </ContentWrapper>
     </Wrapper>
